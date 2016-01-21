eb9fe70eeb68a1aaa461854a7667b1046512970a
Nikolai Petkov <nikipetkov@abv.bg>
2016-01-21 16:24:19 +0200

    Added same simple message on no query

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
+    if (req.url === '/') {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("FCC timestamp microservice\n");
+        res.write("\n");
+        res.write("Example usage:\n");
+        res.write("https://still-cliffs-32544.herokuapp.com/December%2015,%202015\n");
+        res.end("https://still-cliffs-32544.herokuapp.com/1450137600");
+    }
     var reqTime = decodeURIComponent(req.url.slice(1));
 
     // If requested time is number convert it
@@ -20,4 +28,4 @@ var server = http.createServer(function(req, res) {
     }
 });
 
-server.listen(3000);
+server.listen(process.env.PORT || 5000);
